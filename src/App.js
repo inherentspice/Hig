7e95de7d28e77f2d404c42b7678f6abc5a00dbb6
Stephen Smith <stephenlehanesmith@gmail.com>
2022-10-07 16:39:20 +0800

    initialized state for the counters

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [dayQuality, setDayQuality] = useState(
+    {
+      depressing: 0,
+      numb: 0,
+      alright: 0,
+      beautiful: 0
+    })
+
+  function handleClick(e) {
+    const name = e.target.name
+    setDayQuality(prevState => ({...prevState, [name]: prevState[name] + 1}))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>How's it goin?</h1>
-      </header>
-    </div>
+    <>
+    <h1>hows it goin</h1>
+    <button onClick={handleClick} name="depressing">depressing</button>
+    <p>{dayQuality.depressing}</p>
+    <button onClick={handleClick} name="numb">numb</button>
+    <p>{dayQuality.numb}</p>
+    <button onClick={handleClick} name="alright">alright</button>
+    <p>{dayQuality.alright}</p>
+    <button onClick={handleClick} name="beautiful">beautiful</button>
+    <p>{dayQuality.beautiful}</p>
+
+
+
+    </>
   );
 }
 
