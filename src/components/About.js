8623c0fcb3fbebb46170673c89a5f94c4b87fd98
Stephen Smith <stephenlehanesmith@gmail.com>
2022-10-13 18:36:03 +0800

    added styles for each page

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,9 @@
 export default function About() {
   return (
-    <>
-      <h1>about</h1>
-    </>
+      <main style={{backgroundColor: "rgb(55 194 255)"}}>
+        <div className="main--contents" style={{backgroundColor: "white"}}>
+          <h1 style={{color: "rgb(55 194 255)"}}>about</h1>
+        </div>
+      </main>
   )
 }
--- src/components/History.js
+++ src/components/History.js
@@ -1,7 +1,9 @@
 export default function History() {
   return (
-    <>
-      <h1>history</h1>
-    </>
+    <main style={{backgroundColor: "rgb(255, 105, 180)"}}>
+      <div className="main--contents" style={{backgroundColor: "white"}}>
+        <h1 style={{color: "rgb(255, 105, 180)"}}>history</h1>
+      </div>
+    </main>
   )
 }
