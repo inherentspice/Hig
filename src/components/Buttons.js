defb44c5b8682c9d8337de1a1409e13d65f89c3b
Stephen Smith <stephenlehanesmith@gmail.com>
2022-10-17 20:21:05 +0800

    removed counter beneath voting buttons and styled icons in Footer

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -5,15 +5,12 @@ export default function Button(props) {
   <div className="button--display">
       <div className="button--single">
         <button onClick={props.handleClick} name="nah">nah</button>
-        <p>{props.dayQuality.nah}</p>
       </div>
       <div className="button--single">
         <button onClick={props.handleClick} name="alright">alright</button>
-        <p>{props.dayQuality.alright}</p>
       </div>
       <div className="button--single">
         <button onClick={props.handleClick} name="yeah">yeah</button>
-        <p>{props.dayQuality.yeah}</p>
       </div>
     </div>
   )
