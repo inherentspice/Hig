1465d711226a28de24c01a4fa130e8946f3b7221
Stephen Smith <stephenlehanesmith@gmail.com>
2022-10-30 15:15:11 +0800

    changed colour functions so that change in background would be more pronounced

--- src/components/GetColors.js
+++ src/components/GetColors.js
@@ -1,5 +1,5 @@
 
 export default function GetColors(dayQuality) {
-  const color = `rgb(${(dayQuality.nah * 23) % 255}, ${(dayQuality.alright * 52) % 255}, ${(dayQuality.yeah * 27) % 255})`
+  const color = `rgb(${(dayQuality.nah * 23) % 255}, ${(dayQuality.alright * 23) % 255}, ${(dayQuality.yeah * 23) % 255})`
   return color
 }
